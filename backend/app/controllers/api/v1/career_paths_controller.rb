f575a32bbe9467de0146e56ebf54a97bde484b1f
jasmine1226 <jasmine1226@gmail.com>
2020-03-08 21:59:22 -0700

    updated to api/v1

---
+++ backend/app/controllers/api/v1/career_paths_controller.rb
@@ -0,0 +1,53 @@
+module Api::V1
+  class CareerPathsController < ApplicationController
+    before_action :set_career_path, only: [:show, :update, :destroy]
+
+    # GET /career_paths
+    def index
+      @career_paths = CareerPath.all
+
+      render json: @career_paths
+    end
+
+    # GET /career_paths/1
+    def show
+      render json: @career_path
+    end
+
+    # POST /career_paths
+    def create
+      @career_path = CareerPath.new(career_path_params)
+
+      if @career_path.save
+        render json: @career_path, status: :created, location: @career_path
+      else
+        render json: @career_path.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /career_paths/1
+    def update
+      if @career_path.update(career_path_params)
+        render json: @career_path
+      else
+        render json: @career_path.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /career_paths/1
+    def destroy
+      @career_path.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_career_path
+        @career_path = CareerPath.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def career_path_params
+        params.require(:career_path).permit(:title)
+      end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :courses
-  resources :career_paths
+  namespace :api do
+    namespace :v1 do
+      resources :courses
+      resources :career_paths
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
