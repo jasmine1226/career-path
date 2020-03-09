a57ee780a5a1af271681cbe62ccaa774ed7c0138
jasmine1226 <jasmine1226@gmail.com>
2020-03-09 12:23:56 -0700

    set up redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,19 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import CareerPathsReducer from "./reducers/CareerPathsReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(CareerPathsReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ frontend/src/reducers/CareerPathsReducer.js
@@ -0,0 +1,8 @@
+const CareerPathsReducer = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default CareerPathsReducer;
